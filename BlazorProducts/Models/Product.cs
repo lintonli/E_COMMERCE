6263182e39f95c231194ffbe6c08c2c5797dc834
linton <lintonli162@gmail.com>
2024-01-09 20:32:08 +0300

    blazor products

---
+++ BlazorProducts/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorProducts.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Price
+        {
+            get; set;
+        }
+    }
+}
